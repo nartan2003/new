68fdd1623a12909918d18470c5c6ca8f0dae7727
nartan <tyaginartan@gmail.com>
2024-06-13 13:34:24 +0530

    save

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Am from './components/Am';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div >
+      
+      <Counter/>
+      <Am count='4'/>
+    </div>
+    
+  );
+
+}
+
+export default App;
---
+++ my-app/src/components/Am.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Am=(props)=>{
+   
+    return(
+        <>
+        <p>{props.count}  </p>
+        </>
+
+    );
+};
+
+export default Am
